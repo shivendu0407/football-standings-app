cd202d39d18a022afc642eb436e06fe759bfe84a
shivendu0407 <shivendu0407@gmail.com>
2022-07-04 00:21:40 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../App";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>The Football-Standings App 2021&copy;</p>
+    </div>
+  );
+};
+
+export default Footer;
